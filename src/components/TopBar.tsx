6e59fe8d5213b15ddf67707d2050aef04a4bcd41
Vinoth Kirubakaran <vinothefifthstar@hotmail.com>
2025-02-13 13:54:57 -0700

    fix: ci file

--- src/components/TopBar.tsx
+++
@@ -1,9 +0,0 @@
-export default function TopBar() {
-  return (
-    <div className="bg-accent text-white p-4 shadow-md">
-      <div className="container mx-auto">
-        <h1 className="text-xl font-semibold">Email Rules Engine</h1>
-      </div>
-    </div>
-  );
-} 
